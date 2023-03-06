4e427445026e99ee0c5366f063e10bd3cc24211d
Sahil Sharma <sahil.sharma@codibex.com>
2023-03-06 19:48:15 +0530

    Use Store

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,20 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  const github = {
+  const github = useStore( {
     org: 'ys1113457623',
     repos: ['ys1113457623', 'flutter_portfolio','API','Programming-Paradigms'] as string[] | null,
 
-  }
+  });
+  
+  /*
+  store the state of the component
+present the state as a proxy that can observe read/writes to the store
+serialize the state of the store into JSON on application pause.
+observe which properties of the store are used in a component template and create subscriptions to the store. The subscriptions are then used to automatically update the component template if a store changes.
+  */
   
   return (
    <div>
